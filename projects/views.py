4aa397d80f9d6ab7fb9ed771d03f1ae8627a7851
mason <mason.doney@outlook.com>
2023-09-05 14:35:25 -0500

    Feature 13

--- projects/views.py
+++ projects/views.py
@@ -12,3 +12,12 @@ def list_projects(request):
         "projects": projects
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = Project.objects.get(id=id)
+    context = {
+        "project": project,
+    }
+    return render(request, "projects/detail.html", context)
