8d5bd01b5158aefa37ad0d3df14bd8552cbc45b3
mason <mason.doney@outlook.com>
2023-09-05 14:21:19 -0500

    Feature 10

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,14 @@ class LogInForm(forms.Form):
         widget=forms.PasswordInput(),
     )
 
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput(),
+    )
+    password_confirmation = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput(),
+    )
